47f9d1dcdfc1691196be08f4683ca7ba8854ec0f
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-29 23:18:23 -0500

    chore: remove unneeded imports

--- src/components/Nav/NavBar/Navbar.jsx
+++ src/components/Nav/NavBar/Navbar.jsx
@@ -1,10 +1,8 @@
 import { NavLink } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import StyledNavBar from './StyledNavbar';
-import useAuth from '../../../hooks/useAuth';
 
 const Navbar = () => {
-  const { auth } = useAuth();
   const location = useLocation();
   return (
     <StyledNavBar>
