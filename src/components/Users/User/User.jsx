160dfa076c3d62c9fc7b998763d4933b008baa4c
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-01-03 18:39:00 -0500

    delete not working

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,13 +1,55 @@
+import { useState, useEffect } from 'react';
 import StyledUser from './StyledUser';
+import axios from '../../../api/axios';
 
 const User = ({ userInfo }) => {
+  const [errMsg, setErrMsg] = useState('');
+
+  const DELETE_URL = `/users/${userInfo.id}`;
+
+  const betterDate = (str) => {
+    return new Date(str);
+  };
+
+  const createdDate = betterDate(userInfo.created);
+  const updatedDate = betterDate(userInfo.updated);
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    setErrMsg('');
+    try {
+      const res = await axios.delete(DELETE_URL, { withCredentials: true });
+    } catch (err) {
+      const errDat = err.code === 'ERR_NETWORK' ? 'Server error' : err.response.data.msg;
+      console.log(err);
+      setErrMsg(errDat);
+    }
+  };
+
   return (
     <StyledUser>
       <h4>{userInfo.username}</h4>
       <p>
-        <em>Created on: </em>
-        {userInfo.created}
+        {userInfo.firstname} {userInfo.lastname}
       </p>
+      <p>
+        <em>Created on: </em>
+        {`${createdDate.getMonth() + 1}`}
+        <strong>/</strong>
+        {`${createdDate.getDate()}`}
+        <strong>/</strong>
+        {`${createdDate.getFullYear()}`}
+      </p>
+      <p>
+        <em>Updated: </em>
+        {`${updatedDate.getMonth() + 1}`}
+        <strong>/</strong>
+        {`${updatedDate.getDate()}`}
+        <strong>/</strong>
+        {`${updatedDate.getFullYear()}`}
+      </p>
+      <button onClick={handleDelete}>Delete</button>
+      <button>Edit</button>
     </StyledUser>
   );
 };
