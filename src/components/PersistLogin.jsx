9116d4f9eebd98409e8ce336a09c973939672c92
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-01-31 06:54:43 -0500

    refactor: move sign out on refresh with no persist to persist component. better for re-usability

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -1,12 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { useNavigate, Outlet } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import useRefreshToken from '../hooks/useRefreshToken';
 import useAuth from '../hooks/useAuth';
+import useLogout from '../hooks/useLogout';
 
 const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true);
   const refresh = useRefreshToken();
   const { auth, persist } = useAuth();
+  const navigate = useNavigate();
+  const logout = useLogout();
+
+  const signOut = async () => {
+    await logout();
+    navigate('/login');
+  };
 
   useEffect(() => {
     let isMounted = true;
@@ -22,6 +30,9 @@ const PersistLogin = () => {
 
     if (!auth?.accessToken && persist) {
       verifyRefreshToken();
+    } else if (!auth?.accessToken) {
+      setIsLoading(false);
+      signOut();
     } else {
       setIsLoading(false);
     }
--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,18 +1,10 @@
-import { useNavigate, useLocation, Navigate, Outlet } from 'react-router-dom';
+import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 import { jwtDecode } from 'jwt-decode';
-import useLogout from '../hooks/useLogout';
 
 const RequireAuth = () => {
   const { auth } = useAuth();
   const location = useLocation();
-  const navigate = useNavigate();
-  const logout = useLogout();
-
-  const signOut = async () => {
-    await logout();
-    navigate('/login');
-  };
 
   const decoded = auth?.accessToken ? jwtDecode(auth.accessToken) : undefined;
 
@@ -21,7 +13,6 @@ const RequireAuth = () => {
   if (auth?.accessToken && admin) {
     return <Outlet />;
   } else {
-    signOut();
     return (
       <Navigate to="/login" state={{ from: location, failAuth: true, isAdmin: admin }} replace />
     );
