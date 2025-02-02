e85e6f8cd7cf2a8858d032300280ddd7f6f1654a
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-02-02 17:12:41 -0500

    build: update dependancies

--- postinstall.js
+++ postinstall.js
@@ -1,10 +1,15 @@
 import fse from 'fs-extra';
 import path from 'path';
 const topDir = import.meta.dirname;
-fse.emptyDirSync(String(path.join(topDir, 'public', 'tinymce')));
-fse.copySync(
-  String(path.join(topDir, 'node_modules', 'tinymce'), path.join(topDir, 'public', 'tinymce')),
-  {
-    overwrite: true,
-  },
-);
+// fse.emptyDirSync(String(path.join(topDir, 'public', 'tinymce')));
+// fse.copySync(
+//   String(path.join(topDir, 'node_modules', 'tinymce'), path.join(topDir, 'public', 'tinymce')),
+//   {
+//     overwrite: true,
+//   },
+// );
+
+fse.emptyDirSync('./public/tinymce');
+fse.copySync(String('./node_modules/tinymce', './public/tinymce'), {
+  overwrite: true,
+});
