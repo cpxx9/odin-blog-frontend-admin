f5c1b37d2e99cafbf56c42cb2398b2c745deb43d
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-02-02 21:21:42 -0500

    feat: prod api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://blogapi.cjplabs.com/api';
 
 export default axios.create({
   baseURL: BASE_URL,
